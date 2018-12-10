4d50ec1fded08bbda13f38592a4da1b2d3655869
Salim <salimboumediene@gmail.com>
2018-12-10 20:18:22 +0100

    custom email confirmation

--- app/mailers/forest_mailer.rb
+++ app/mailers/forest_mailer.rb
@@ -1,24 +1,13 @@
 class ForestMailer < ActionMailer::Base
     
-    def contact(user)
-        email = { :from_email   => "salimboumediene@gmail.com",
-        :from_name    => "Salim",
-        :subject      => "Hello",
-        :text_part    => "Hi",
-        :recipients   => [{:email => "#{user}"}] }
-
-        test = Mailjet::Send.create(email)
-
-        # retrieve the API response
-        p test.attributes['Sent']
-    end
-
-    def awesome_mail(user)
-
+    def welcome_email(user)
+        headers['X-MJ-CustomID'] = 'custom value'
+        headers['X-MJ-EventPayload'] = 'custom payload'
+    
         mail(
-          to: user.email,
-          delivery_method_options: { api_key: "#{Rails.application.credentials.api_public_key_mail}",
-                                     secret_key: "#{Rails.application.credentials.api_secret_key_mail}" }
+        from: "salimboumediene@gmail.com",
+        to: "#{user}",
+        subject: "This is a nice welcome email"
         )
       end
 
