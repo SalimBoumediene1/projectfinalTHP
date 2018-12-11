988370ad6093e2b4fb345a57378848c719040a03
Salim <salimboumediene@gmail.com>
2018-12-11 18:48:32 +0100

    niente

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      ForestMailer.welcome_email("#{current_user.email}").deliver_now
-    else
-      puts "pas signed"
-    end
+    
   end
 end
