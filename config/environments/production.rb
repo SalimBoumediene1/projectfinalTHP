eb1face739da1af3065f86d7540a85ab7d4b1a5c
Salim <salimboumediene@gmail.com>
2018-12-16 18:51:48 +0100

    config smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   end
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'shielded-eyrie-68443.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'guarded-earth-49014.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
