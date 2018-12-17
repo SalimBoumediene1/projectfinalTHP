be4b29d69fd4233a7d595f5f1aad77cd20ce9f02
Salim <salimboumediene@gmail.com>
2018-12-17 11:04:56 +0100

    ajout d'image dans le mail

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,37 +1,27 @@
 class ChargesController < ApplicationController
-    def new
-    end
-  
-  def create
-    @amount = (current_user.cart.total_price * 100).to_i
-  
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
-  
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
-  
-    new_order = Order.new
-    new_order.user_id = current_user.id
-    new_order.save
-  
-    current_user.cart.cart_items.each do |cart_item|
-      ItemOrder.create(order_id: new_order.id, quantity: cart_item.quantity, item_id: cart_item.item_id)
-      cart_item.destroy
-    end
-
-    ForestMailer.welcome_email(current_user.email).deliver_now
-  
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+  def new
   end
   
-end
+  def create
+  @order = Order.create!(user_id: current_user.id)
+  current_user.cart.items.each do |item|
+    @order.items << item
+  end
   
+  current_user.cart.items.clear
+  @current_email = current_user.email
+  ForestMailer.welcome_email(current_user.email,@order).deliver_now
+  @amount = 500
+  customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken])
+    charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Cat pics',
+    :currency    => 'eur')
+rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to :root
+  end
+end
--- app/mailers/forest_mailer.rb
+++ app/mailers/forest_mailer.rb
@@ -1,13 +1,14 @@
 class ForestMailer < ActionMailer::Base
     
-    def welcome_email(user)
+    def welcome_email(user, order)
+        @order = order
         headers['X-MJ-CustomID'] = 'custom value'
         headers['X-MJ-EventPayload'] = 'custom payload'
     
         mail(
         from: "salimboumediene@gmail.com",
         to: "#{user}",
-        subject: "This is khkgjfgjf email"
+        subject: "This is order email"
         )
       end
 
